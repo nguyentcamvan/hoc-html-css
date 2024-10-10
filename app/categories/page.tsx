3dcbff038fde300891a42889966069819c6eb723
nguyentcamvan <nguyentcamvan11@dtu.edu.vn>
2024-10-10 22:21:17 +0700

    updtae cateogory

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const CategoryPage = () => {
     return (
-        <div className='bg-amber-700'>Noi dung se duoc hien thi trong trang CategoryPage</div>
+        <div className='bg-amber-700'>Noi dung se duoc hien thi trong trang CategoryPage1</div>
+       
     )
 }
 
