fcd39bc6720238198c985f0c3e788dfaa7663a2a
nguyentcamvan <nguyentcamvan11@dtu.edu.vn>
2024-10-10 22:13:01 +0700

    first commit

---
+++ app/categories/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CategoryPage = () => {
+    return (
+        <div className='bg-amber-700'>Noi dung se duoc hien thi trong trang CategoryPage</div>
+    )
+}
+
+export default CategoryPage;
